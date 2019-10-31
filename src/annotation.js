54ca024570508d13f2bdf752d087aae75da712ee
mike <mikus.elksnitis@gmail.com>
2019-10-31 10:07:22 +0200

    changes

--- src/annotation.js
+++ src/annotation.js
@@ -6,11 +6,14 @@ goog.require('annotorious.shape');
  * A 'domain class' implementation of the external annotation interface.
  * @param {string} src the source URL of the annotated object
  * @param {string} text the annotation text
+ * @param {string} tag the annotation tag
  * @param {annotorious.shape.Shape} shape the annotated fragment shape
  * @constructor
  */
+
 annotorious.Annotation = function(src, text, shape, tag) {
-  this.tag = tag || "";
+  console.log(tag);
+  this.tag = tag || '';
   this.src = src;
   this.text = text;
   this.shapes = [ shape ];
